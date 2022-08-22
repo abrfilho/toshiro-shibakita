23d09fe0502644682b030df8e6aed332604b8bbf
Adilton B. R. Filho <54344422+abrfilho@users.noreply.github.com>
2022-08-22 18:59:23 -0300

    Atualizar index.php
    Modificando atributos para execução em minha instância.

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ header('Content-Type: text/html; charset=iso-8859-1');
 
 echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
-$servername = "54.234.153.24";
+$servername = "18.228.211.239";
 $username = "root";
-$password = "Senha123";
-$database = "meubanco";
+$password = "123";
+$database = "DIO";
 
 // Criar conexão
 
@@ -34,7 +34,7 @@ $valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
 $host_name = gethostname();
 
 
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
+$query = "INSERT INTO dados (ID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
 
 
 if ($link->query($query) === TRUE) {
